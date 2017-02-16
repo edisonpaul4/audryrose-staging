d77f25f33c9bbae46b78ce61816596190948eaa2
Jeremy Adam <male@jeremyadam.com>
2017-02-16 09:10:04 -0600

    fixed bug with variant color grouping and added other options

--- src/components/ProductVariants.js
+++ src/components/ProductVariants.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Input, Divider, Button } from 'semantic-ui-react';
+import { Table, Input, Button, Dropdown, Dimmer, Segment, Loader } from 'semantic-ui-react';
 import classNames from 'classnames';
 // import numeral from 'numeral';
 // import moment from 'moment';
@@ -7,14 +7,31 @@ import classNames from 'classnames';
 class VariantRow extends Component {
 	render() {  	
 		const data = this.props.data;
+		let otherOptions = [];
+		if (data.gemstone_value) otherOptions.push(data.gemstone_value);
+		if (data.singlepair_value) otherOptions.push(data.singlepair_value);
+		if (data.letter_value) otherOptions.push(data.letter_value);
+		if (data.length_value) otherOptions.push(data.length_value);
+		if (data.font_value) otherOptions.push(data.font_value);
     return (
       <Table.Row>
         <Table.Cell>asdf</Table.Cell>
         <Table.Cell>{data.color_value ? data.color_value : ''}</Table.Cell>
         <Table.Cell>{data.size_value ? data.size_value : 'OS'}</Table.Cell>
+        <Table.Cell>{otherOptions ? otherOptions.join(', ') : null}</Table.Cell>
 				<Table.Cell><Input type='number' transparent defaultValue={0} /></Table.Cell>
 				<Table.Cell className='right aligned'>[not loaded yet]</Table.Cell>
-				<Table.Cell className='right aligned'>[not loaded yet]</Table.Cell>
+				<Table.Cell className='right aligned'>
+          <Button.Group color='grey' size='mini' compact>
+            <Button content='Order' />
+            <Dropdown floating button compact className='icon'>
+              <Dropdown.Menu>
+                <Dropdown.Item icon='exchange' text='Resize' />
+                <Dropdown.Item icon='hide' text='Hide' />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Button.Group>
+				</Table.Cell>
       </Table.Row>
     );
   }
@@ -40,15 +57,15 @@ class VariantsTable extends Component {
 		}
 		const tableTitle = (this.props.title) ? <h3>{this.props.title}</h3> : null;
     return (
-      <div>
-        <Divider />
+      <Segment>
         {tableTitle}
-        <Table className='variants-table' compact size='small' columns={6}>
+        <Table className='variants-table' basic='very' compact size='small' columns={6}>
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>Style-Color</Table.HeaderCell>
               <Table.HeaderCell>Color</Table.HeaderCell>
               <Table.HeaderCell sorted="descending">Size</Table.HeaderCell>
+              <Table.HeaderCell>Other Options</Table.HeaderCell>
               <Table.HeaderCell>ACT OH</Table.HeaderCell>
               <Table.HeaderCell className='right aligned'>RETAIL $</Table.HeaderCell>
               <Table.HeaderCell className='right aligned'>Actions</Table.HeaderCell>
@@ -58,7 +75,7 @@ class VariantsTable extends Component {
             {variantRows}
           </Table.Body>
         </Table>
-      </div>
+      </Segment>
     );
   }
 }
@@ -85,23 +102,29 @@ class ProductVariants extends Component {
 		);
 		var variantsTables = [];
 		if (variants) {
+  		console.log(this.props.data.name + " total variants: " + variants.length);
   		let variantGroupings = [];
   		// Determine variant groupings
 			variants.map(function(variantItem, i) {
-  			const color = (variantItem.color_value) ? {color_label: variantItem.color_label, color_value: variantItem.color_value} : null;
-  			if (color && variantGroupings.indexOf(color) < 0) variantGroupings.push(color);
+  			const color = (variantItem.color_value) ? variantItem.color_value : null;
+  			
+  			if (color && variantGroupings.indexOf(color) < 0) {
+    			console.log('push ' + color);
+    			variantGroupings.push(color);
+  			}
 				return variantItem;
 	    });
+	    console.log(this.props.data.name + " total groupings: " + variantGroupings.length);
 	    
 	    if (variantGroupings.length > 0) {
   	    // If there are groupings, create a VariantsTable for each group
   	    variantGroupings.map(function(variantGroup, i) {
     	    var variantsInGroup = [];
     	    variants.map(function(variantItem, j) {
-      	    if (variantItem.color_value === variantGroup.color_value) variantsInGroup.push(variantItem);
+      	    if (variantItem.color_value === variantGroup) variantsInGroup.push(variantItem);
       	    return variantItem;
     	    });
-    	    variantsTables.push(<VariantsTable variants={variantsInGroup} title={variantGroup.color_value} key={i} />);
+    	    variantsTables.push(<VariantsTable variants={variantsInGroup} title={variantGroup} key={i} />);
     	    return variantGroup;
   	    });
 	    } else {
@@ -112,9 +135,13 @@ class ProductVariants extends Component {
     return (
       <Table.Row className={rowClass}>
         <Table.Cell colSpan='12' className='variant-row'>
-          <Button icon='refresh' floated='right' size='mini' content='Reload' loading={this.props.isReloading} onClick={()=>this.handleReloadClick(this.props.data.productId)} />
-          <Divider horizontal>Inventory</Divider>
-          {variantsTables}
+          <Button icon='refresh' size='mini' content='Reload' loading={this.props.isReloading} onClick={()=>this.handleReloadClick(this.props.data.productId)} />
+          <Dimmer.Dimmable as={Segment} vertical blurring dimmed={this.props.isReloading}>
+            <Dimmer active={this.props.isReloading} inverted>
+              <Loader>Loading</Loader>
+            </Dimmer>
+            {variantsTables}
+          </Dimmer.Dimmable>
         </Table.Cell>
       </Table.Row>
     );
