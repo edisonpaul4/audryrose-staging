5434c1a77c3a77114a2875c718eb89a05a486e51
Alfonso Roman Zubeldia <alfonsoromanz24@gmail.com>
2018-08-13 09:26:44 -0300

    LS-198 / Table Sorting

--- src/products-stats/containers/ProductsByDesignerStatsContainer.jsx
+++ src/products-stats/containers/ProductsByDesignerStatsContainer.jsx
@@ -30,6 +30,82 @@ class ProductsByDesignerStatsContainer extends React.Component {
       this.props.getProductStatsByDesigner(data.value);
     }
     
+    sortedProducts(stats, type) {
+        const sorter = (a, b, desc = false) => {
+            if (!desc) {
+                if (a > b) return 1;
+                else return -1;
+                // else return 0;
+            } else {
+                if (a < b) return 1;
+                else return -1;
+                // else return 0;
+            }
+        };
+
+        switch (type) {
+            case 'product_name_asc':
+                return stats.sort((a, b) => sorter(a.productName.trim().toLowerCase(), b.productName.trim().toLowerCase()));
+
+            case 'product_name_desc':
+                return stats.sort((a, b) => sorter(a.productName.trim().toLowerCase(), b.productName.trim().toLowerCase(), true));
+
+            case 'checked_in_asc':
+                return stats.sort((a, b) => sorter(a.checkedIn, b.checkedIn));
+
+            case 'checked_in_desc':
+                return stats.sort((a, b) => sorter(a.checkedIn, b.checkedIn, true));
+
+            case 'shipped':
+                return stats.sort((a, b) => sorter(a.shipped, b.shipped));
+
+            case 'shipped':
+                return stats.sort((a, b) => sorter(a.shipped, b.shipped, true));
+
+            case 'on_hand_asc':
+                return stats.sort((a, b) => sorter(a.onHand, b.onHand));
+
+            case 'on_hand_desc':
+                return stats.sort((a, b) => sorter(a.onHand, b.onHand, true));
+
+            case 'discrepancy_asc':
+                return stats.sort((a, b) => sorter(a.discrepancy, b.discrepancy));
+
+            case 'discrepancy_desc':
+                return stats.sort((a, b) => sorter(a.discrepancy, b.discrepancy, true));
+
+            case 'ordered_all_time_asc':
+                return stats.sort((a, b) => sorter(a.orderedAllTime, b.orderedAllTime));
+
+            case 'ordered_all_time_desc':
+                return stats.sort((a, b) => sorter(a.orderedAllTime, b.orderedAllTime, true));
+
+            default:
+                return stats;
+        }
+    }
+    
+    onGetIcon(type) {
+        const sortQuery = this.props.location.query.sort ? this.props.location.query.sort : '';
+        if (!sortQuery.includes(type))
+            return null;
+        else if (sortQuery.includes('asc'))
+            return <Icon name="triangle up" />
+        else if (sortQuery.includes('desc'))
+            return <Icon name="triangle down" />
+        else
+            return null;
+    }
+    
+    onChangeSort(type) {
+        const sortQuery = this.props.location.query.sort ? this.props.location.query.sort : '';
+        const queryString = `?sort=${type}_${sortQuery.includes('asc') ? 'desc' : 'asc'}`;
+
+        this.props.router.push({
+            pathname: this.props.location.pathname,
+            search: queryString
+        });
+    }
   
     render() {
       return (
@@ -51,28 +127,46 @@ class ProductsByDesignerStatsContainer extends React.Component {
                       <Table celled>
                           <Table.Header>
                               <Table.Row>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'product_name')}>
                                       Product name
+                                      {this.onGetIcon('product_name')}
                                   </Table.HeaderCell>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'checked_in')}>
                                       Checked In
+                                      {this.onGetIcon('checked_in')}
                                   </Table.HeaderCell>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'shipped')}>
                                       Shipped
+                                      {this.onGetIcon('shipped')}
                                   </Table.HeaderCell>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'on_hand')}>
                                       On Hand
+                                      {this.onGetIcon('on_hand')}
                                   </Table.HeaderCell>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'discrepancy')}>
                                       Discrepancy
+                                      {this.onGetIcon('discrepancy')}
                                   </Table.HeaderCell>
-                                  <Table.HeaderCell>
+                                  <Table.HeaderCell
+                                    className="pointer"
+                                    onClick={this.onChangeSort.bind(this, 'ordered_all_time')}>
                                       Sold (All time)
+                                      {this.onGetIcon('ordered_all_time')}
                                   </Table.HeaderCell>
                               </Table.Row>
                           </Table.Header>
                           <Table.Body>
-                              {this.props.productStats.productsStatsByDesigner.map(ps => (
+                              {this.sortedProducts(this.props.productStats.productsStatsByDesigner, this.props.location.query.sort).map(ps => (
                                   <Table.Row key={'key-' + ps.productId}>
                                       <Table.Cell>
                                           <a href={`/products/search?q=${ps.productId}`}>
